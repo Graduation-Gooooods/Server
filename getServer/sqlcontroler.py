0fcd3c7ce1766fc58aaa2a6496fe78ef4c2dd959
Lee Changwon <89180067+LeeChangwon-AT-SCH@users.noreply.github.com>
2022-04-27 17:00:37 +0900

    Update sqlcontroler.py

--- getServer/sqlcontroler.py
+++ getServer/sqlcontroler.py
@@ -1,57 +1,28 @@
-#############################################
-# 2022-04-24
-# Get data from database
-##############################################
+import pymysql
+import pymysql.cursors
 
-import json
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import urllib.parse as urlparser
-import sqlControler
+connection = pymysql.connect(host='localhost', user='test_root', password='20171522',
+                            db='test_tb', charset='utf8', cursorclass=pymysql.cursors.DictCursor) # connenct db
+cursor = connection.cursor()
 
-cont_type = 'Content-type'
-jsontype = 'Application/json'
-host = '192.168.0.14'
-port = 2017
-
-class requestHandler(BaseHTTPRequestHandler):
-    def _set_headers(self): # 200 Okay header 
-        self.send_response(200)
-        self.send_header(cont_type, jsontype)
-        self.end_headers()
+class sqlControler():
+    def __init__(self):
+        print("Maked sql controler")
         
-    def do_GET(self):
-        querypath = urlparser.urlparse(self.path)
-        path = querypath.path  
-        user_path = path.split('/')[1]
-        print(user_path)
-        
-        if user_path == 'app': # If Sender is App
-            if path.endswith('/getauto'):
-                body = json.dumps(sqlControler.sqlControler.getAuto())
-                self._set_headers()
-                self.wfile.write(body.encode())
-        
-        elif user_path == 'jamjam': # If Sender is Jamjam
-            if path.endswith('/priviousmode'):
-                body = json.dumps(sqlControler.sqlControler.getPriviousMode())
-                self._set_headers()
-                self.wfile.write(body.encode())
-            
-        else: # Not App or Jamjam
-            self.send_response(404)
-            self.end_headers()
-            
-                   
-    def do_POST(self):
-        querypath = urlparser.urlparse(self.path)
-        path, query = querypath.path, querypath.query
-        print(path, query)
-        
-        if path.endswith('/createauto'):
-            sqlControler.sqlControler.createAuto(scc=10, tm=160, cs=4, md=1)
-            self._set_headers()
-            dic = {"rsp": "success"}
-            self.wfile.write(json.dumps(dic).encode())
-            
-        
-HTTPServer((host, port), requestHandler).serve_forever()
+       
+    def getData():
+        sql = "SELECT * FROM _intergration ORDER BY date_time  DESC LIMIT 10;"
+        cursor.execute(sql) # send sql line to db server 
+        data = cursor.fetchall()  # fetchall, fetchone, fetchmany...
+        return data
+    
+    def createAuto(scc, cs, tm, md):
+        sql = "INSERT INTO _automatic(scc, cs, tm, md) VALUES(%s, %s, %s, %s)"
+        cursor.execute(sql, (scc, cs, tm, md)) # send sql line to db server 
+        complete = connection.commit()
+        print(complete)
+        return complete
+    
+    def getPriviousMode():
+        pass   
+    
